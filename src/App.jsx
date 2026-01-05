2bf0d1c908ccd344e778bfeac551101312b1d1d7
sanjay poudel <poudelsanjay93@gmail.com>
2026-01-05 20:24:06 +0545

    added the tabs section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Header from './components/header.jsx'
 import Footer from './components/footer.jsx'
 import questions from './Assets/faqs.js' 
+import tabsection from './Assets/tabs.js'
 
 function App() {
   
@@ -11,14 +12,14 @@ function App() {
     return(
       <FAQitem key={i} index={v.id} question={v.question} answer={v.answer} />
     )
-
-  
   })
 
   return (
     
     <div className="faq">
       <Header />
+
+        <Tabitem />
         
         {toogle}
 
@@ -40,4 +41,29 @@ function FAQitem({index, question, answer, Itemevent, itemdetails}){
       <p className={status ? "answer-open" : ""}>{answer}</p>
     </div>
   )
+}
+
+function Tabitem(){
+  let [status, setStatus]= useState(0);
+  let [content,setcontent]= useState(tabsection[0]);
+
+  let buttonstatus=(i)=>{
+    setStatus(i);
+    setcontent(tabsection[i]);
+    
+  }
+
+  return(
+    <div className='tabs'>
+      <div className='tab-buttons'>
+        {tabsection.map((content,i)=>{
+          return(
+            <button key={i}  onClick={() => buttonstatus(i)} className={ status == i ? "tab-btn active" : "tab-btn"}>{content.title}</button>
+          )
+        })}
+      </div>
+      <p>{content.description}</p>
+    </div>
+    
+    )
 }
